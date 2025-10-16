66a5a53742f243fc6814122e8350204895f2be85
joseph <Moble@DESKTOP-4RK8H3Q>
2025-10-16 13:51:09 +0900

    마스터 백업

--- InventoryManager/resource.h
+++ InventoryManager/resource.h
@@ -47,13 +47,6 @@
 #define IDC_LIST1                       1044
 #define IDC_LIST2                       1050
 #define IDC_PLACE_STATS                 1051
-#define IDC_STATIC_GROUP                4060
-#define IDC_EDIT_DB_HOST                4061
-#define IDC_EDIT_DB_PORT                4062
-#define IDC_EDIT_DB_NAME                4063
-#define IDC_EDIT_DB_USER                4064
-#define IDC_EDIT_DB_PASS                4065
-#define IDC_BTN_SAVE_DB                 4066
 #define IDC_STATIC_PERIOD_LABEL         1058
 #define IDC_DATETIMEPICKER_START        1059
 #define IDC_DATETIMEPICKER_END          1060
@@ -75,6 +68,14 @@
 #define IDC_EDIT_WARNING2               4057
 #define IDC_EDIT_DANGER                 4058
 #define IDC_STATIC_WARNING              4059
+#define IDC_STATIC_GROUP                4060
+#define IDC_EDIT_DB_HOST                4061
+#define IDC_EDIT_DB_PORT                4062
+#define IDC_EDIT_DB_NAME                4063
+#define IDC_EDIT_DB_USER                4064
+#define IDC_EDIT_DB_PASS                4065
+#define IDC_BTN_SAVE_DB                 4066
+#define IDC_EDIT_DB_HOST2               4067
 
 // Next default values for new objects
 // 
